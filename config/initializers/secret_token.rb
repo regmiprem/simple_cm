cfa51ed48b3a3394ee4ab7931a8ffca13bdcb4eb
regmiprem <regmiprem101@gmail.com>
2012-06-18 13:09:55 +0545

    config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCm::Application.config.secret_token = '5cca973dc7489471b080a3ee125d7979aa57a455e741962c606f34df518be5cc40577d4107ad51c211627f115a73702abc53180d1d75bb2b74b11b4354dc37cc'
